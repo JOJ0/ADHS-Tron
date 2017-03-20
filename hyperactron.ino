dc8012cb77700533985f75f938b40d68229bf5d2
J0J0 T <jojo@peek-a-boo.at>
2017-03-20 18:27:36 +0100

    simple legato mode using 2 counters
      * each note on or off is counted
      * only release gate when on and off counters are equal
      * DEBUG out both counters
      * some comment cleanups around gPitchAnalog formula versions
      * noterange const moved to very beginning -> user configurable stuff

--- hyperactron.ino
+++ hyperactron.ino
@@ -2,22 +2,24 @@
 #define USBserial Serial
 #define MIDIserial Serial1
 
+// Ableton C-2 = C0 = 00, Ableton C2 = C0 = 24
+const uint8_t LOWEST_KEY = 00; // 24=C2, 36=C3
+const uint8_t HIGHEST_KEY = 48; // 84=C7, 72=C6, 60=C5, 48=C4, 
+
 int LedInt = 13;
 int PinGate = 2; // digital
 //int PinCutoff = 3; // PWM 977 Hz FIXME Teensy?
 int PinCutoff = 3;
 //int PinPitch = 18; // PWM 480 Hz -> change to 0x01 -> 31372.55 FIXME Teensy?
 int PinPitch = A14;
+
 //bool gMidiGateOn = false;
-uint8_t gMidiNoteValue = 0;
+//uint8_t gMidiNoteValue = 0;
 uint16_t gPitchAnalog = 0;
-
-uint16_t gPitchAnalog_last = 0;
-uint16_t gPitchAnalog_curr = 0;
-
-// Ableton C-2 = C0 = 00, Ableton C2 = C0 = 24
-const uint8_t LOWEST_KEY = 00; // 24=C2, 36=C3
-const uint8_t HIGHEST_KEY = 48; // 84=C7, 72=C6, 60=C5, 48=C4, 
+//uint16_t gPitchAnalog_last = 0;
+//uint16_t gPitchAnalog_curr = 0;
+uint8_t gNoteOnCounter = 0;
+uint8_t gNoteOffCounter = 0;
 
 // MIDI settings struct
 struct MySettings : public midi::DefaultSettings {
@@ -44,14 +46,15 @@ void debugNote (byte channel, byte pitch, byte velocity, uint16_t PitchAnalog) {
 void handleNoteOn(byte Channel, byte PitchMidi, byte Velocity) {
   if (PitchMidi >= LOWEST_KEY && PitchMidi <= HIGHEST_KEY) {
     //gPitchAnalog_last = gPitchAnalog_curr;
+    gNoteOnCounter++;
     // nostromo teensy + dac mcp4822
     //gPitchAnalog = uint16_t((gMidiNoteValue-LOWEST_KEY)*835.666666666); // + gMidiPitchBend ;  // 8191/12
     // arduino uno (Konstante? 255 * 0.08333333 / 5 = 42.49999983)
-    //gPitchAnalog = uint16_t((PitchMidi-LOWEST_KEY)*255*0.083333333/5);
+    //gPitchAnalog = uint16_t((PitchMidi-LOWEST_KEY)*255*0.083333333/5);// 0.0833333=1/12V
     // teensy 3.2
-    //gPitchAnalog = uint16_t((PitchMidi-LOWEST_KEY)*4095*0.083333333/3.3); // 0.055 3.3V=5oct
-    gPitchAnalog = uint16_t((PitchMidi-LOWEST_KEY)*4095*0.02666/3.3); // 0.0833333=1/12V
-    //gPitchAnalog = uint16_t((PitchMidi-LOWEST_KEY)*4095*0.0277777778/3.3); // 0.0833333=1/12V
+    //gPitchAnalog = uint16_t((PitchMidi-LOWEST_KEY)*4095*0.083333333/3.3); // 0.055=1st if 3.3V=5oct
+    gPitchAnalog = uint16_t((PitchMidi-LOWEST_KEY)*4095*0.02666/3.3); // 0.002666 WORKING, FIXME why?
+    //gPitchAnalog = uint16_t((PitchMidi-LOWEST_KEY)*4095*0.0277777778/3.3);
     // mpasserini formula test:  unsigned int in_pitch = dac_max / notes_max * (inNote - notes_lowest);
                                //note_stack.push( in_pitch );
     //gPitchAnalog = uint16_t(4095 / HIGHEST_KEY * (PitchMidi - LOWEST_KEY));
@@ -60,13 +63,22 @@ void handleNoteOn(byte Channel, byte PitchMidi, byte Velocity) {
     digitalWrite(LedInt, HIGH); // LED on
     digitalWrite(PinGate, HIGH); // GATE on
     debugNote(Channel, PitchMidi, Velocity, gPitchAnalog); // DEBUG
+    USBserial.print("NoteOn - gNoteOnCounter: "); USBserial.print(gNoteOnCounter); // DEBUG
+    USBserial.print(", gNoteOffCounter: "); USBserial.println(gNoteOffCounter); // DEBUG
   }
 }
 
-void handleNoteOff(byte channel, byte pitch, byte velocity) { // NoteOn messages with 0 velocity are interpreted as NoteOffs. 
-  digitalWrite(LedInt, LOW);
-  digitalWrite(PinGate, LOW);
-  analogWrite(PinPitch, 0);
+void handleNoteOff(byte channel, byte pitch, byte velocity) { // NoteOn with 0 velo is NoteOff. 
+  gNoteOffCounter++;
+  if (gNoteOnCounter == gNoteOffCounter) {
+    gNoteOnCounter = 0;
+    gNoteOffCounter = 0;
+    digitalWrite(LedInt, LOW);
+    digitalWrite(PinGate, LOW);
+    analogWrite(PinPitch, 0);
+  }
+  USBserial.print("NoteOff - gNoteOnCounter: "); USBserial.print(gNoteOnCounter); // DEBUG
+  USBserial.print(", gNoteOffCounter: "); USBserial.println(gNoteOffCounter); // DEBUG
 }
 
 void setup() {
